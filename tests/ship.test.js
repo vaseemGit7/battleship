a535e3113c91536efb7f901f25ad1f762b4440ef
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2025-01-05 00:35:25 +0530

    test(ship): add test to validate Ship initialization with specified size

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -4,3 +4,8 @@ test("Ship class is initialized", () => {
   const newShip = new Ship();
   expect(newShip).toBeInstanceOf(Ship);
 });
+
+test("A Ship of size 5 is created", () => {
+  const newShip = new Ship(5);
+  expect(newShip.size).toBe(5);
+});
