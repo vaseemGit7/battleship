b93e0d74e2906fa4ce6c042470b634b9052d87b0
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2025-01-05 13:44:55 +0530

    test(ship): refactor tests to reduce redundancy

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,21 +1,23 @@
 import Ship from "../src/classes/Ship.js";
 
+let newShip;
+
+beforeEach(() => {
+  newShip = new Ship(5);
+});
+
 test("Ship class is initialized", () => {
-  const newShip = new Ship();
   expect(newShip).toBeInstanceOf(Ship);
 });
 
 test("A Ship of size 5 is created", () => {
-  const newShip = new Ship(5);
   expect(newShip.size).toBe(5);
 });
 
 test("should initialize hit property with zero", () => {
-  const newShip = new Ship(5);
   expect(newShip.hits).toBe(0);
 });
 
 test("should initialize isSunk property with false", () => {
-  const newShip = new Ship(5);
   expect(newShip.isSunk).toBe(false);
 });
