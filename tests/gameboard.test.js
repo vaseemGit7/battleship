c8aeba5d209361ada5b84cdc32e71384eaf97852
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2025-01-06 18:25:51 +0530

    test(gameboard): import Gameboard class for instance test

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,3 +1,5 @@
+import Gameboard from "../src/classes/Gameboard";
+
 test("should initialize a gameboard instance", () => {
   const gameboard = new Gameboard();
   expect(gameboard).toBeInstanceOf(Gameboard);
