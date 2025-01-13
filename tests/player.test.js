34a2ae5505dc4f5fe241f553404f57f18cb31f2b
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2025-01-13 20:47:37 +0530

    test(player): add test to verify type of instance to be human

--- tests/player.test.js
+++ tests/player.test.js
@@ -4,3 +4,8 @@ test("should initialize a player instance", () => {
   const player = new Player();
   expect(player).toBeInstanceOf(Player);
 });
+
+test("should initialize a player instance with type (human)", () => {
+  const player = new Player("human");
+  expect(player.type).toBe("human");
+});
