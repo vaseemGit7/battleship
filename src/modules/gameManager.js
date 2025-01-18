b7e5f2c1a98356225f4b4ddee3fe419e282745e2
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2025-01-18 17:44:18 +0530

    feat(gameManager): populate players board with predetermined coords

--- src/modules/gameManager.js
+++ src/modules/gameManager.js
@@ -1,4 +1,5 @@
 import Player from "../classes/Player";
+import Ship from "../classes/Ship";
 
 export default gameManager = (() => {
   let playerOne, playerTwo;
@@ -6,5 +7,23 @@ export default gameManager = (() => {
   const initializeGame = () => {
     playerOne = new Player("human");
     playerTwo = new Player("computer");
+
+    let carrier = new Ship(5);
+    let battleship = new Ship(4);
+    let cruiser = new Ship(3);
+    let submarine = new Ship(3);
+    let destroyer = new Ship(2);
+
+    playerOne.board.placeShip(destroyer, { x: 1, y: 6 }, "horizontal");
+    playerOne.board.placeShip(submarine, { x: 3, y: 0 }, "vertical");
+    playerOne.board.placeShip(cruiser, { x: 0, y: 2 }, "horizontal");
+    playerOne.board.placeShip(battleship, { x: 6, y: 6 }, "vertical");
+    playerOne.board.placeShip(carrier, { x: 4, y: 4 }, "horizontal");
+
+    playerTwo.board.placeShip(destroyer, { x: 3, y: 3 }, "horizontal");
+    playerTwo.board.placeShip(submarine, { x: 8, y: 4 }, "horizontal");
+    playerTwo.board.placeShip(cruiser, { x: 4, y: 6 }, "horizontal");
+    playerTwo.board.placeShip(battleship, { x: 5, y: 1 }, "vertical");
+    playerTwo.board.placeShip(carrier, { x: 1, y: 1 }, "horizontal");
   };
 })();
