03b13be9f63434011378f74d1a54de9933d29bb2
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2025-01-18 20:42:12 +0530

    feat(display): add functionlity to render players gameboard

--- src/modules/displayManager.js
+++ src/modules/displayManager.js
@@ -1,3 +1,34 @@
 const displayManager = (() => {
-  return {};
+  const boards = document.querySelectorAll(".player-board");
+  const playerOneBoard = document.querySelector("#playerOneBoard");
+  const playerTwoBoard = document.querySelector("#playerTwoBoard");
+
+  boards.forEach(
+    (board) =>
+      (board.style.cssText =
+        "border: 2px solid black; max-width: 400px; display: grid; grid-template-rows: repeat(10,1fr); margin: 20px;"),
+  );
+
+  const renderBoard = (player, playerBoard) => {
+    for (let x = 0; x < playerBoard.size; x++) {
+      const gridRow = document.createElement("div");
+      gridRow.style.cssText =
+        "display: grid; grid-template-columns: repeat(10,1fr)";
+      for (let y = 0; y < playerBoard.size; y++) {
+        const gridCol = document.createElement("div");
+        gridCol.setAttribute("data-index-x", x);
+        gridCol.setAttribute("data-index-y", y);
+        gridCol.style.cssText = "border: 1px solid black; height: 40px";
+        gridRow.appendChild(gridCol);
+      }
+      player === "playerOne"
+        ? playerOneBoard.appendChild(gridRow)
+        : playerTwoBoard.appendChild(gridRow);
+    }
+  };
+  return {
+    renderBoard,
+  };
 })();
+
+export default displayManager;
