d0a7b4e5f98f067e6e3ae6974eb29e50b120dcb6
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2025-01-19 22:48:09 +0530

    feat(event): add event handler functionality for player attack

--- src/modules/eventController.js
+++ src/modules/eventController.js
@@ -1,5 +1,28 @@
+import gameManager from "./gameManager";
+
 const eventController = (() => {
-  return {};
+  const init = () => {
+    const gameboardCells = document.querySelectorAll(".gameboard-cell");
+
+    gameboardCells.forEach((cell) =>
+      cell.addEventListener("click", () => {
+        let x = cell.getAttribute("data-index-x");
+        let y = cell.getAttribute("data-index-y");
+
+        _handleAttack({ x: x, y: y });
+      }),
+    );
+  };
+
+  const _handleAttack = (coords) => {
+    let opponentBoard = gameManager.getOpponentPlayer().board;
+    gameManager.playTurn(coords);
+
+    console.log("hit");
+    console.log(opponentBoard);
+  };
+
+  return { init };
 })();
 
 export default eventController;
