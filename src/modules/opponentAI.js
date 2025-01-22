abd257cf1f2e0d2221d42bffde033b9b313684dc
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2025-01-22 21:35:53 +0530

    feat(ai): add functionality to get random attacks

--- src/modules/opponentAI.js
+++ src/modules/opponentAI.js
@@ -1,5 +1,25 @@
-const opponenetAI = (() => {
-  return {};
+const opponentAI = (() => {
+  const getRandomAttack = (board) => {
+    let isValid = false;
+    let coords;
+
+    while (!isValid) {
+      let x = Math.floor(Math.random() * 10);
+      let y = Math.floor(Math.random() * 10);
+      console.log(x, y, board.getCellState({ x: x, y: y }));
+      if (
+        board.getCellState({ x: x, y: y }) === null ||
+        board.getCellState({ x: x, y: y }).status === "intact"
+      ) {
+        console.log("within if: ", x, y, board.getCellState({ x: x, y: y }));
+        coords = { x: x, y: y };
+        isValid = true;
+      }
+    }
+    return coords;
+  };
+
+  return { getRandomAttack };
 })();
 
-export default opponenetAI;
+export default opponentAI;
