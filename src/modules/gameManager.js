380a296fe3f15dbdcba7804b8e3b54663a86bbe4
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2025-01-22 23:50:13 +0530

    fix(gameManager): create seperate ship instances for each player

--- src/modules/gameManager.js
+++ src/modules/gameManager.js
@@ -14,23 +14,29 @@ const gameManager = (() => {
     currentPlayer = playerOne;
     opponentPlayer = playerTwo;
 
-    let carrier = new Ship(5);
-    let battleship = new Ship(4);
-    let cruiser = new Ship(3);
-    let submarine = new Ship(3);
-    let destroyer = new Ship(2);
+    let carrierOne = new Ship(5);
+    let battleshipOne = new Ship(4);
+    let cruiserOne = new Ship(3);
+    let submarineOne = new Ship(3);
+    let destroyerOne = new Ship(2);
 
-    playerOne.board.placeShip(destroyer, { x: 1, y: 6 }, "horizontal");
-    playerOne.board.placeShip(submarine, { x: 3, y: 0 }, "vertical");
-    playerOne.board.placeShip(cruiser, { x: 0, y: 2 }, "horizontal");
-    playerOne.board.placeShip(battleship, { x: 6, y: 6 }, "vertical");
-    playerOne.board.placeShip(carrier, { x: 4, y: 4 }, "horizontal");
+    let carrierTwo = new Ship(5);
+    let battleshipTwo = new Ship(4);
+    let cruiserTwo = new Ship(3);
+    let submarineTwo = new Ship(3);
+    let destroyerTwo = new Ship(2);
 
-    playerTwo.board.placeShip(destroyer, { x: 3, y: 3 }, "horizontal");
-    playerTwo.board.placeShip(submarine, { x: 8, y: 4 }, "horizontal");
-    playerTwo.board.placeShip(cruiser, { x: 4, y: 6 }, "horizontal");
-    playerTwo.board.placeShip(battleship, { x: 5, y: 1 }, "vertical");
-    playerTwo.board.placeShip(carrier, { x: 1, y: 1 }, "horizontal");
+    playerOne.board.placeShip(destroyerOne, { x: 1, y: 6 }, "horizontal");
+    playerOne.board.placeShip(submarineOne, { x: 3, y: 0 }, "vertical");
+    playerOne.board.placeShip(cruiserOne, { x: 0, y: 2 }, "horizontal");
+    playerOne.board.placeShip(battleshipOne, { x: 6, y: 6 }, "vertical");
+    playerOne.board.placeShip(carrierOne, { x: 4, y: 4 }, "horizontal");
+
+    playerTwo.board.placeShip(destroyerTwo, { x: 3, y: 3 }, "horizontal");
+    playerTwo.board.placeShip(submarineTwo, { x: 8, y: 4 }, "horizontal");
+    playerTwo.board.placeShip(cruiserTwo, { x: 4, y: 6 }, "horizontal");
+    playerTwo.board.placeShip(battleshipTwo, { x: 5, y: 1 }, "vertical");
+    playerTwo.board.placeShip(carrierTwo, { x: 1, y: 1 }, "horizontal");
 
     displayManager.renderBoard("playerOne", playerOne.board);
     displayManager.renderBoard("playerTwo", playerTwo.board);
