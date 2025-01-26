7e6fb06aff350abadafe810c067eb1b1223b97d8
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2025-01-27 00:07:28 +0530

    feat(event): validate ship placement and integrate module functions

--- src/modules/eventController.js
+++ src/modules/eventController.js
@@ -11,11 +11,9 @@ const eventController = (() => {
     });
   };
 
-  const initializeDragEvents = () => {
+  const initializeDragEvents = (playerBoard) => {
     const vessel = document.querySelector(".vessel");
     const gameboardCells = document.querySelectorAll(".gameboard-cell");
-
-    console.log(vessel, gameboardCells);
 
     vessel.addEventListener("dragstart", () => {
       console.log("Drag Started");
@@ -31,10 +29,34 @@ const eventController = (() => {
       cell.addEventListener("drop", (e) => {
         e.preventDefault();
 
-        let x = cell.getAttribute("data-index-x");
-        let y = cell.getAttribute("data-index-y");
+        let x = parseInt(cell.getAttribute("data-index-x"));
+        let y = parseInt(cell.getAttribute("data-index-y"));
 
         console.log("Dropped at", "x :", x, " y :", y);
+
+        let vessel = gameManager.getCurrentVessel();
+
+        if (
+          _validatePlacement(
+            playerBoard.board,
+            { x: x, y: y },
+            vessel.size,
+            "horizontal",
+          )
+        ) {
+          gameManager.updateShipPlacement({ x: x, y: y }, "horizontal");
+          displayManager.renderBoard("setup", playerBoard);
+
+          if (!gameManager.checkAllShipsPlaced()) {
+            console.log("triggered");
+            displayManager.renderVessel();
+            initializeDragEvents(playerBoard);
+          } else {
+            console.log("All ships placed!");
+          }
+        } else {
+          console.log("Invalid placement");
+        }
       }),
     );
   };
@@ -58,6 +80,27 @@ const eventController = (() => {
 
     console.log("hit");
     console.log(opponentBoard);
+  };
+
+  const _validatePlacement = (board, coords, size, orientation) => {
+    if (orientation === "horizontal" && coords.x + size > board.size)
+      return false;
+    if (orientation === "vertical" && coords.y + size > board.size)
+      return false;
+
+    for (let i = 0; i < size; i++) {
+      if (
+        (orientation === "horizontal" &&
+          (board[coords.x][coords.y + i] >= board.size ||
+            board[coords.x][coords.y + i] !== null)) ||
+        (orientation === "vertical" &&
+          (board[coords.x + i][coords.y] >= board.size ||
+            board[coords.x + i][coords.y] !== null))
+      ) {
+        return false;
+      }
+    }
+    return true;
   };
 
   return { init, initializeDragEvents, handlePlayerInput };
